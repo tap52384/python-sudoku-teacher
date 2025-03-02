5e3d6320dc801ba4ab695a7d7346d67757fc0888
Z. Patrick Lewis <647983+tap52384@users.noreply.github.com>
2025-03-02 00:25:35 -0500

    add ability to validate a grid string

--- constants.py
+++ constants.py
@@ -9,3 +9,13 @@ EMPTY = 0
 
 # Used to determine the max 1-based index of a cell
 MAX_CELL_INDEX = (MAX_NUMBER * MAX_NUMBER) - 1
+
+# Valid non-empty cell values
+VALID_VALUES = set(range(EMPTY, MAX_NUMBER + 1))
+
+# When reading a file or an input string representing a sudoku grid, we need to
+# accept 0, '.', '_', and ' ' as empty cells
+EMPTY_CELLS = {'0', '.', '_', ' '}
+
+# Valid input values for importing a sudoku grid
+VALID_INPUT_VALUES = VALID_VALUES.union(EMPTY_CELLS)
--- grid.py
+++ grid.py
@@ -1,11 +1,20 @@
-from constants import MIN_NUMBER, MAX_NUMBER
+from constants import MIN_NUMBER, MAX_NUMBER, VALID_INPUT_VALUES
 
 class Grid:
     """
     Represents the playing grid of a sudoku board.
     """
-    def __init__(self):
+    def __init__(self, grid_string=None):
+        """
+        Initializes a new grid with the given string.
+        """
         self._cells = [None] * (MAX_NUMBER * MAX_NUMBER)
+
+        # If a grid string is provided, we need to validate it and raise an
+        # exception if it is invalid
+        if grid_string:
+            self._validate_grid_string(grid_string)
+            self._initialize_grid(grid_string)
 
     def __str__(self):
         output = ""
@@ -14,3 +23,21 @@ class Grid:
                 output += f"{self.getCell(i + 1, j + 1)} "
             output += "\n"
         return output
+
+    def _validate_grid_string(self, grid_string):
+        """
+        Validates the given grid string.
+        """
+
+        # Remove all whitespace from the ends of the string
+        grid_string = grid_string.strip()
+
+        # Check if the grid string is the correct length
+        if len(grid_string) != (MAX_NUMBER * MAX_NUMBER):
+            raise ValueError("Invalid grid string length")
+
+        # Check if the grid string contains only valid characters
+        if not all(char in VALID_INPUT_VALUES for char in grid_string):
+            raise ValueError("Invalid characters in grid string")
+
+        self._grid_string = grid_string
